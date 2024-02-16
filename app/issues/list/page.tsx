80c2e1d103a9f2641b7b6ac7fb35976e468b0733
StanoievDragan <kamisugames@gmail.com>
2024-02-17 01:17:39 +0200

    bug fixing

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -1,29 +1,26 @@
 import React from 'react';
-import {Table} from '@radix-ui/themes';
-import {IssueStatusBadge, Link} from '@/app/components';
+import { Table } from '@radix-ui/themes';
+import { IssueStatusBadge, Link } from '@/app/components';
 import prisma from "@/prisma/client";
-import delay from "delay";
-import IssueActions from "@/app/issues/list/IssueActions";
-import {Issue, Status} from "@prisma/client";
+import { Issue, Status } from "@prisma/client";
 import NextLink from "next/link";
-import {ArrowUpIcon} from "@radix-ui/react-icons";
-import Pagination from "@/app/components/Pagination";
+import { ArrowUpIcon } from "@radix-ui/react-icons";
+import IssueActions from "@/app/issues/list/IssueActions";
 
 interface Props {
-    searchParams: {status: Status, orderBy: keyof Issue, page: string}
+    searchParams: { status: Status, orderBy: keyof Issue, page: string }
 }
 
-const IssuesPage = async ({searchParams}: Props) => {
-
+const IssuesPage = async ({ searchParams }: Props) => {
     const columns: {
         label: string;
         value: keyof Issue;
         className?: string;
     }[] = [
-        {label: 'Issue', value: 'title'},
-        {label: 'Status', value: 'status', className: "hidden md:table-cell"},
-        {label: 'Created', value: 'createdAt', className: "hidden md:table-cell"}
-    ]
+        { label: 'Issue', value: 'title' },
+        { label: 'Status', value: 'status', className: "hidden md:table-cell" },
+        { label: 'Created', value: 'createdAt', className: "hidden md:table-cell" }
+    ];
 
     const statuses = Object.values(Status);
     const status = statuses.includes(searchParams.status) ? searchParams.status : undefined;
@@ -31,23 +28,15 @@ const IssuesPage = async ({searchParams}: Props) => {
     const orderBy = columns
         .map(column => column.value)
         .includes(searchParams.orderBy)
-        ? {[searchParams.orderBy]: "asc"}
+        ? { [searchParams.orderBy]: "asc" }
         : undefined;
 
-    const where = {status};
-
-    const page = parseInt(searchParams.page) || 1;
-    const pageSize = 10;
+    const where = { status };
 
     const issues = await prisma.issue.findMany({
         where,
         orderBy,
-        skip: (page - 1) * pageSize,
-        take: pageSize
-
-    })
-
-    const issueCount = prisma.issue.count({ where})
+    });
 
     return (
         <div>
@@ -55,12 +44,14 @@ const IssuesPage = async ({searchParams}: Props) => {
             <Table.Root variant={'surface'}>
                 <Table.Header>
                     <Table.Row>
-                        {columns.map(column => <Table.ColumnHeaderCell key={column.value} className={column.className}>
-                            <NextLink href={{
-                                query: {...searchParams, orderBy: column.value}
-                            }}>{column.label}</NextLink>
-                            {column.value === searchParams.orderBy && <ArrowUpIcon className={"inline"}></ArrowUpIcon>}
-                        </Table.ColumnHeaderCell>)}
+                        {columns.map(column => (
+                            <Table.ColumnHeaderCell key={column.value} className={column.className}>
+                                <NextLink href={{
+                                    query: { ...searchParams, orderBy: column.value }
+                                }}>{column.label}</NextLink>
+                                {column.value === searchParams.orderBy && <ArrowUpIcon className={"inline"}></ArrowUpIcon>}
+                            </Table.ColumnHeaderCell>
+                        ))}
                     </Table.Row>
                 </Table.Header>
                 <Table.Body>
@@ -70,20 +61,19 @@ const IssuesPage = async ({searchParams}: Props) => {
                                 <Link href={`/issues/${issue.id}`}>
                                     {issue.title}
                                 </Link>
-                                <div className={'block md:hidden'}><IssueStatusBadge status={issue.status}/></div>
+                                <div className={'block md:hidden'}><IssueStatusBadge status={issue.status} /></div>
                             </Table.Cell>
                             <Table.Cell className={'hidden md:table-cell'}><IssueStatusBadge
-                                status={issue.status}/></Table.Cell>
+                                status={issue.status} /></Table.Cell>
                             <Table.Cell className={'hidden md:table-cell'}>{issue.createdAt.toDateString()}</Table.Cell>
                         </Table.Row>
                     ))}
                 </Table.Body>
             </Table.Root>
-            
         </div>
     );
 };
 
-export const dynamic = 'force-dynamic'
+export const dynamic = 'force-dynamic';
 
 export default IssuesPage;
